78b0923a2a5470b824914c6bc2e01a6c13a5557d
Jen Simmons <jen@jensimmons.com>
2012-04-30 15:17:12 +0100

    further refinement to get the js starter files worked out

--- ponySTARTERKIT/js/scripts.js
+++ ponySTARTERKIT/js/scripts.js
@@ -1,1 +1,12 @@
-// Put your custom js here. If it's not working, that's likely because you haven't used `drupal-specific special functions. Meh. Sorry, you'll have to go look those up.
+// Put your custom js here. If it's not working, that's likely because you haven't used Drupal-specific special functions. Meh. Sorry, you'll have to go look those up.
+
+// For jQuery, you need this:
+
+(function ($) {
+  $(document).ready(function(){
+  	
+    // Put your jQuery functions here
+    // and here
+    
+  });
+})(jQuery);
